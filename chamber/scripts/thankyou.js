7bca8c55540bc061156df25da85ace30ed0784fe
Raagnaa <egarcia4@byupathway.edu>
2026-06-04 16:21:09 -0500

    week4 try2

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -8,9 +8,11 @@ const summary = document.querySelector("#summary");
 summary.innerHTML = `
     <p><strong>First Name:</strong> ${params.get("fname")}</p>
     <p><strong>Last Name:</strong> ${params.get("lname")}</p>
+    <p><strong>Organization Title:</strong> ${params.get("title")}</p>
     <p><strong>Email:</strong> ${params.get("email")}</p>
     <p><strong>Mobile:</strong> ${params.get("phone")}</p>
-    <p><strong>Business:</strong> ${params.get("business")}</p>
+    <p><strong>Organization:</strong> ${params.get("organization")}</p>
+    <p><strong>Description:</strong> ${params.get("description")}</p>
     <p><strong>Membership:</strong> ${params.get("membership")}</p>
     <p><strong>Submitted:</strong> ${params.get("timestamp")}</p>
 `;
